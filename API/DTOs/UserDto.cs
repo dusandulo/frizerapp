e14bddd417c41c05997481ffb37ebffa76fee1ec
Bugarina <bugarinovicaleksa01@gmail.com>
2025-02-10 20:57:31 +0100

    Added a possibility to choose whether you are a Client or a Stylist when registering

--- API/DTOs/UserDto.cs
+++ API/DTOs/UserDto.cs
@@ -2,15 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Enums;
 
 namespace API.DTOs
 {
     public class UserDto
-    {
-        public int Id { get; set; }
-        public string Email { get; set; }
-        public string Name { get; set; }
-        public string Token { get; set; }
-        public bool IsUserVerified { get; set; }
-    }
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Token { get; set; }
+    public bool IsUserVerified { get; set; }
+    public RoleEnum Role { get; set; } 
+}
 }
